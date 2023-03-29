f945ab57c2cb7ef6d698eed2b8a49a46e848e2c7
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-03-29 10:48:34 +0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -27,7 +27,7 @@ import EditContact, { action as editAction } from './routes/Edit'
         loader: contactLoader,
       },
       {
-        path: "contacts/:contactId/Edit",
+        path: "contacts/:contactId/edit",
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
