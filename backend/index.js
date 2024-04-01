b36956b0e04100aecacf3cead1337ed924c2577b
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-01 07:51:10 +0300

    databse connection created

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const mysql = require('mysql2');
 
 const app = express();
 
-const PORT = 6000;
+const PORT = process.env.PORT;
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE
+});
+
+app.get("/", (req, res) => {
+    res.json("Backend Server running")
+})
 
 app.listen(PORT, () => {
     console.log('Connected to backend!');
