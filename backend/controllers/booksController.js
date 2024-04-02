040b43b8b82d7f2e8da9f52a3b1b51582fa07f00
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-02 16:32:02 +0300

    books controller created, separation of concerns

---
+++ backend/controllers/booksController.js
@@ -0,0 +1,19 @@
+const db = require('../app');
+const booksRouter = require('express').Router();
+
+booksRouter.get("/api/books", (req, res) => {
+    res.json("Backend Server running")
+});
+
+booksRouter.get("/api/books", (req, res) => {
+    const getBooks = "SELECT * FROM books";
+
+    db.query(getBooks, (err, data) => {
+        if (err) return res.json(err);
+
+        return res.json(data);
+    })
+
+});
+
+module.exports = booksRouter;
