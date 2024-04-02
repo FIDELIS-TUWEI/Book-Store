4be7cc594e2d2e5222a341b35cc01287dbef4f46
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-02 16:43:19 +0300

    separation of concerns cleanup

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,7 @@
-const config = require('./utils/config');
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
 const middleware = require('./utils/middleware');
 const booksRouter = require('./controllers/booksController');
-
-
-const db = mysql.createConnection({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DATABASE
-});
 
 app.use(express.json());
 app.use(middleware.requestLogger);
@@ -20,7 +10,4 @@ app.disable("x-powered-by");
 app.use("/api/books", booksRouter);
 app.use(middleware.unknownEndpoint);
 
-module.exports = {
-    app,
-    db
-};
+module.exports = app;
