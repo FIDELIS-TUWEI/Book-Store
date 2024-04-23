a3c2cd14cab4ae365397b4030a934027ff53a187
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-23 17:41:37 +0300

    cors initialized

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const middleware = require('./utils/middleware');
 const booksRouter = require('./controllers/booksController');
 
 app.use(express.json());
+app.use(cors());
 app.use(middleware.requestLogger);
 app.disable("x-powered-by");
 
-app.use(booksRouter);
+app.use("/api/v1", booksRouter);
 app.use(middleware.unknownEndpoint);
 
 module.exports = app;
