8b285c8d1d9f5294cfb0ead0f952c13821c9f063
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-23 17:41:15 +0300

    display all books from mysql databse backend server

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Books = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            try {
+                const res = await axios.get(`http://localhost:5000/api/v1/books`)
+                setBooks(res.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchBooks();
+    }, [])
   return (
-    <div>Books</div>
+    <div>
+        <h1>MySQL Book Shop</h1>
+        <div className="books">
+            {books.map((book) => (
+                <div className="bookCard" key={book.id}>
+                    {book.cover && <img src={book.cover} alt="books" />}
+                    <h2>{book.title}</h2>
+                    <p>{book.desc}</p>
+                    <span>{book.price}</span>
+                </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
