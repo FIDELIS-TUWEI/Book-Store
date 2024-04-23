9f731f01032443c76449263db6f5d502f739034e
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-23 18:36:38 +0300

    backend logic to delete book with id

--- backend/controllers/booksController.js
+++ backend/controllers/booksController.js
@@ -33,6 +33,18 @@ booksRouter.post('/books', (req, res) => {
 
         return res.json("Book has been created successfully");
     })
-})
+});
+
+// route to delete book with id
+booksRouter.delete('/books/:id', (req, res) => {
+    const booksId = req.params.id;
+
+    const q = "DELETE FROM  books WHERE id = ?";
+
+    db.query(q, [booksId], (err, data) => {
+        if (err) return res.json(err);
+        return res.json("Book has been deleted succesfully");
+    })
+});
 
 module.exports = booksRouter;
