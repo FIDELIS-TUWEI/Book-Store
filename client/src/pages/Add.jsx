46882ab6af81045a0bc8311dffc17a5c40fd8aa4
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2024-04-23 18:03:24 +0300

    Logic to add a new book and navigate to all books complete

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,7 +1,45 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
+  const navigate = useNavigate();
+
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: ""
+  });
+
+
+  const handleChange = (e) => {
+    setBook(((prev => ({ ...prev, [e.target.name]: e.target.value }))));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(`http://localhost:5000/api/v1/books`, book);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>Add</div>
+    <div className="form">
+      <h1>Add New Book Form</h1>
+      <form>
+        <input type="text" placeholder="title" onChange={handleChange} name="title" />
+        <input type="text" placeholder="desc" onChange={handleChange} name="desc" />
+        <input type="number" placeholder="price" onChange={handleChange} name="price" />
+        <input type="text" placeholder="cover" onChange={handleChange} name="cover" />
+
+        <button onClick={handleClick}>Add</button>
+      </form>
+    </div>
   )
 }
 
